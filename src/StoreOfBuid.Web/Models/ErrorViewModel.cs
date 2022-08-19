684153b99b680aacbe552ddac2d3fb4e54474087
Bruno Ramos Pires <bruno.rp45@gmail.com>
2022-08-18 21:39:25 -0300

    Correção

---
+++ src/StoreOfBuid.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StoreOfBuid.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
